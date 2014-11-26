0a93e0525c1b761f6fa6078be558bfac398bef55
whats0n <mrdvv@mail.ru>
2014-11-26 13:07:21 +0200

    done/fix

--- js/valik.js
+++ js/valik.js
@@ -5,7 +5,7 @@ head.ready(function() {
 	$('.search__keyword').focusout(function(event) {
 		$('.search__label').removeClass('js-active');
 	});
-	//message textarea
+	//search
 	$('.search__keyword').on('keyup', function(event) {
 	 if($(this).val() == '') {
 		$(this).parent().find('.search__delete').addClass('js-not');
@@ -15,6 +15,18 @@ head.ready(function() {
 	});
 	$('.search__delete').on('click', function() {
 		$('.search__keyword').val('');
+		$(this).addClass('js-not');
+	});
+	//request-search
+	$('.request-top__keyword').on('keyup', function(event) {
+	 if($(this).val() == '') {
+		$(this).parent().find('.request-top__delete').addClass('js-not');
+	 } else {
+		$(this).parent().find('.request-top__delete').removeClass('js-not');
+	 }
+	});
+	$('.request-top__delete').on('click', function() {
+		$('.request-top__keyword').val('');
 		$(this).addClass('js-not');
 	});
 });
